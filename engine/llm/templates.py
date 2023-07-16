ec6b1b690e3e7264bfe72656440b73ef13d24c4a
Juan Soriano <juanky.soriano@gmail.com>
2023-07-16 07:56:01 +0200

    Updat story lenght

--- engine/llm/templates.py
+++ engine/llm/templates.py
@@ -6,9 +6,9 @@ You are the narrator.
 The story is always about a crime, the main character (the player) is a person involved somehow in the crime and who has to solve it either because of professional reasons or very strong personal reasons.
 
 Output a JSON with the following fields:
-    - "text": the next short paragraph of the story. No more than 280 characters. Narrated on the style of Truman Capote's In Cold Blood. Add title. Use Markdown syntax. E.g title in bold as *title*. E.g italic for thoughts or quotes. VERY IMPORTANT
+    - "text": the next short paragraph of the story. Between 300 and 400 characters. Narrated on the style of Truman Capote's In Cold Blood. Add title. Use Markdown syntax. E.g title in bold as *title*. E.g italic for thoughts or quotes. VERY IMPORTANT
     - "image": a DALL·E prompt; text description to generate the image on a text-to-image model
-    - "available_actions": a list of strings with the available actions for the player to take. It's a very super sentence describing the action.
+    - "available_actions": a list of strings with the available actions for the player to take. It's a very short sentence describing the action. Maximum 3 items.
 
 The resulting JSON is:
 """
@@ -31,9 +31,9 @@ Current progress of the story is {percentage}. Make the story progress according
     2. The crime is not resolved. Give an exit to the main character. For example, the main character dies, or is fired, or gets arrested, or the murderer scapes forever and they are never seen again...
 
 Output a JSON with the following fields:
-    - "text": the next short paragraph of the story. No more than 280 characters. Narrated on the style of Truman Capote's In Cold Blood. Use Markdown syntax. E.g italic for thoughts or quotes. VERY IMPORTANT
+    - "text": the next short paragraph of the story. Between 300 and 400 characters.. Narrated on the style of Truman Capote's In Cold Blood. Use Markdown syntax. E.g italic for thoughts or quotes. VERY IMPORTANT
     - "image": a DALL·E prompt; text description to generate the image on a text-to-image model
-    - "available_actions": a list of strings with the available actions for the player to take. It's a super short sentence describing the action.
+    - "available_actions": a list of strings with the available actions for the player to take. It's a very short sentence describing the action. Maximum 3 items.
 
 The resulting JSON is:
 """
